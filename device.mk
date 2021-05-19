c4f2d18f8fbb5e6fe76510832386b7f9d2518ccc
systemad <dannesuke@gmail.com>
2021-05-19 20:09:58 +0200

    Add certificate for oneplus and pixelexperience

--- device.mk
+++ device.mk
@@ -72,3 +72,8 @@ PRODUCT_PACKAGES_ENG += \
 # Apex libraries
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+
+# OEM otacert
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/oneplus \
+    $(LOCAL_PATH)/security/pixelexperience \
